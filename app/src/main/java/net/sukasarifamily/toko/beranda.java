5ff7d15f178330b66793b76f83cf558ef0783c2f
Mas Taka <yanyanruhyana@gmail.com>
2019-01-19 14:42:51 +0700

    Initial commit

---
+++ app/src/main/java/net/sukasarifamily/toko/beranda.java
@@ -0,0 +1,13 @@
+package net.sukasarifamily.toko;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class beranda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beranda);
+    }
+}
